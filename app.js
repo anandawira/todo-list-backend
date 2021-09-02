d011a150afe5866bfea542ae947c90cb36c3b8af
Ananda Wiradharma <anandawira754@gmail.com>
2021-09-02 15:05:26 +0700

    add mongoDB connection

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+mongoose.connect(process.env.MONGO_DB, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
 
 const app = express();
 
