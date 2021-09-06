68c92d8d43572efb8d796e64112b407532915c5c
Ananda Wiradharma <anandawira754@gmail.com>
2021-09-06 15:47:25 +0700

    add validator

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -1,6 +1,12 @@
-exports.activity_create = (req, res, next) => {
-  res.send('POST');
-};
+const Activity = require('../models/activity');
+const User = require('../models/user');
+const { body, validationResult } = require('express-validator');
+
+exports.activity_create = [
+  body('title')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('Title CANNOT be empty'),
 exports.activity_list = (req, res, next) => {
   res.send('GET');
 };
