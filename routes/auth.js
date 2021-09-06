bd925af66b5d59f4ecb3ccabc67df8b171b2462c
Ananda Wiradharma <anandawira754@gmail.com>
2021-09-06 10:09:55 +0700

    change /login method to post.

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const auth_controller = require('../controllers/authController');
 
 router.post('/sign-up', auth_controller.user_create);
 
-router.get('/login', auth_controller.user_login);
+router.post('/login', auth_controller.user_login);
 
 router.get('/access-token', auth_controller.user_refresh_token);
 
