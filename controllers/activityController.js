78b34e4f7f1307be24dde6ec426cbd0ceb1ebe7f
Ananda Wiradharma <anandawira754@gmail.com>
2021-09-06 16:54:44 +0700

    add activity done

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -1,12 +1,52 @@
 const Activity = require('../models/activity');
 const User = require('../models/user');
 const { body, validationResult } = require('express-validator');
+const mongoose = require('mongoose');
 
 exports.activity_create = [
   body('title')
-    .trim()
+    .exists()
+    .withMessage("'title' not found in form data")
     .isLength({ min: 1 })
     .withMessage('Title CANNOT be empty'),
+  body('description')
+    .exists()
+    .withMessage("'description' not found in form data"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const activity = new Activity({
+      _id: new mongoose.Types.ObjectId(),
+      title: req.body.title,
+      description: req.body.description,
+    });
+
+    activity.save((err) => {
+      if (err) {
+        return next(err);
+      }
+
+      User.findByIdAndUpdate(
+        req.user.id,
+        {
+          $addToSet: { activities: activity._id },
+        },
+        (err) => {
+          if (err) {
+            return next(err);
+          }
+
+          return res
+            .status(201)
+            .json({ message: 'Activity created successfully' });
+        },
+      );
+    });
+  },
+];
 exports.activity_list = (req, res, next) => {
   res.send('GET');
 };
