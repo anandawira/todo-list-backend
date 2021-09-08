a4e1f8190c8dfa45960555acb78bcabe8a16520e
Ananda Wiradharma <anandawira754@gmail.com>
2021-09-08 20:29:09 +0700

    add commenting to authenticate middleware

--- middlewares/authenticateToken.js
+++ middlewares/authenticateToken.js
@@ -1,16 +1,23 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
+  // Extract access token from header
   const authHeader = req.headers.authorization;
   const accessToken = authHeader && authHeader.split(' ')[1];
 
+  // If access token not found return 401
   if (accessToken === undefined) return res.sendStatus(401);
+
+  // Verify access token
   jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    // Check if error
     if (err) {
+      // Check if error is not because of expired token then return 401
       if (!err.expiredAt) {
-        return res.status(403).json({ message: err.message });
+        return res.status(401).json({ message: err.message });
       }
 
+      // Check if user sent refresh token. if not then return 403
       if (req.get('refresh_token') === undefined) {
         return res.status(403).json({
           message:
@@ -18,22 +25,33 @@ module.exports = function (req, res, next) {
         });
       }
 
+      // Check if the refresh token is valid
       jwt.verify(
         req.get('refresh_token'),
         process.env.REFRESH_TOKEN_SECRET,
         (err, user) => {
-          if (err) return res.status(403).json({ message: err.message });
+          // If error return 403
+          if (err)
+            return res
+              .status(403)
+              .json({ message: 'refreshing access token failed' });
           const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
             expiresIn: '1d',
           });
-          
+
+          // Add new token to the header
           res.append('new_access_token', accessToken);
           req.user = user;
+
+          // go to next middleware
           return next();
         },
       );
     } else {
+      // Add user from the token to the request object
       req.user = user;
+
+      // go to next middleware
       return next();
     }
   });
